4aee87cdd2c40a23b80da7ede5c7a1969f0e6e9e
kktay281 <steventaykk@stevens-macbook-air.local>
2018-02-07 11:38:20 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,15 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Steven Tay on 05/02/2018.
+//  Copyright © 2018 Xiu Design. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
